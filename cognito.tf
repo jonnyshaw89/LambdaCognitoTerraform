74dff331c46e23024b64309f2586469bcb70c5b5
Jon Shaw <jon.shaw@jassoft.co.uk>
2016-10-18 17:52:00 +0100

    comment identitypool-roles

--- cognito.tf
+++ cognito.tf
@@ -6,8 +6,8 @@ resource "null_resource" "create-identitypool" {
   }
 }
 
-resource "null_resource" "create-identitypool-roles" {
-  provisioner "local-exec" {
-    command = "aws cognito-identity set-identity-pool-roles --identity-pool-id $IDENTITY_POOL_ID --roles unauthenticated=arn:aws:iam::${var.aws_account_id}:role/${aws_iam_role.Cognito_LambdAuthUnauth_Role},authenticated=arn:aws:iam::${var.aws_account_id}:role/${aws_iam_role.Cognito_LambdAuthAuth_Role} --region ${var.aws_region}"
-  }
-}
+//resource "null_resource" "create-identitypool-roles" {
+//  provisioner "local-exec" {
+//    command = "aws cognito-identity set-identity-pool-roles --identity-pool-id $IDENTITY_POOL_ID --roles unauthenticated=arn:aws:iam::${var.aws_account_id}:role/${aws_iam_role.Cognito_LambdAuthUnauth_Role},authenticated=arn:aws:iam::${var.aws_account_id}:role/${aws_iam_role.Cognito_LambdAuthAuth_Role} --region ${var.aws_region}"
+//  }
+//}
