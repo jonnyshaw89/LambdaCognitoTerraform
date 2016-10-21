35a909852f40e4d3972fd00b78e290afe3707778
Jon Shaw <jon.shaw@jassoft.co.uk>
2016-10-21 14:10:41 +0100

    added auth api gateway

---
+++ api-gateway-auth.tf
@@ -0,0 +1,47 @@
+§// API Gateway
+
+// /auth
+resource "aws_api_gateway_resource" "Auth" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  parent_id = "${var.aws_api_gateway_resource_parent}"
+  path_part = "auth"
+}
+
+// /auth
+resource "aws_api_gateway_resource" "Signup" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  parent_id = "${var.aws_api_gateway_resource_parent}"
+  path_part = "signup"
+}
+
+
+// /signup POST
+resource "aws_api_gateway_method" "signup-POST" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.Signup.id}"
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "Auth-createAuth-integration" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.Auth.id}"
+  http_method = "${aws_api_gateway_method.signup-POST.http_method}"
+  type = "AWS_PROXY"
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.createUser.arn}/invocations"
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_method_response" "Auth-POST-200" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.Auth.id}"
+  http_method = "${aws_api_gateway_method.signup-POST.http_method}"
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "Auth-POST-Integration-Response" {
+  rest_api_id = "${var.aws_api_gateway_rest_api_id}"
+  resource_id = "${aws_api_gateway_resource.Auth.id}"
+  http_method = "${aws_api_gateway_method.signup-POST.http_method}"
+  status_code = "${aws_api_gateway_method_response.Auth-POST-200.status_code}"
+}
--- main.tf
+++ main.tf
@@ -5,3 +5,6 @@ provider "aws" {
 variable "aws_region" {}
 variable "aws_account_id" {}
 variable "aws_cognito_identity_pool_id" {}
+
+variable "aws_api_gateway_rest_api_id" {}
+variable "aws_api_gateway_resource_parent" {}
